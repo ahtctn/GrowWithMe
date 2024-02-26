91e2a82264b277f15b14d7f9a904932f3946b3ea
Ahmet Ali Çetin <ahtctn@gmail.com>
2024-02-26 16:37:05 +0300

    Charts

--- GrowWithMe/View/StatsView/StatsView.swift
+++ GrowWithMe/View/StatsView/StatsView.swift
@@ -10,30 +10,77 @@ import Charts
 
 struct StatsView: View {
     @ObservedObject var viewModel = PhysicalDataViewModel()
+    @State private var isAddDataButtonTapped: Bool = false
+    
+    var sortedData: [PhysicalDataModel] {
+        return viewModel.data.sorted(by: { $0.weight > $1.weight })
+    }
+    
     var body: some View {
         NavigationStack {
             List {
                 Section("Weight") {
                     Chart {
-                        ForEach(viewModel.data) { data in
-                            BarMark(x: PlottableValue.value("Height", "\(data.weight) kg"), y: .value("Weight", data.date))
+                        ForEach(sortedData.prefix(12)) { data in
+                            LineMark(x: PlottableValue.value("Date", data.date), y: .value("Weight","\( data.weight) kg"))
+                                .symbol(by: .value("Weight", "Weight/ Date"))
+                                .lineStyle(StrokeStyle(lineWidth: 4, dash: [10, 10]))
                         }
                     }
+                    
                     .padding()
+                    .frame(height: 300)
                     
                 }
                 
                 Section("Length") {
                     Chart {
-                        ForEach(viewModel.data) { data in
-                            BarMark(x: PlottableValue.value("Length", "\(data.length) m"), y: .value("Date", data.date))
+                        ForEach(sortedData.prefix(12)) { data in
+                            LineMark(x: PlottableValue.value("Date", data.date),
+                                     y: .value("Length", "\(data.length)m"))
+                            .symbol(by: .value("Length", "Length/ Date"))
+                            .lineStyle(StrokeStyle(lineWidth: 4, dash: [10, 10]))
+                            
                         }
                     }
                     .padding()
+                    .frame(height: 300)
                     
                 }
+                
+                Section("BMI") {
+                    Chart {
+                        ForEach(sortedData.prefix(12)) { data in
+                            let bmi = viewModel.calculateBMI(weight: data.weight, height: data.length)
+                            
+                            LineMark(x: PlottableValue.value("Date", data.date), y: .value("BMI", String(format: "%.2f", bmi)))
+                                .symbol(by: .value("BMI", "BMI/ Date"))
+                                .lineStyle(StrokeStyle(lineWidth: 4, dash: [10, 10]))
+                            
+                            
+                        }
+                    }
+                    .padding()
+                    .frame(height: 300)
+                }
+                
+                .sheet(isPresented: $isAddDataButtonTapped){
+                    AddDataView(dismiss: {isAddDataButtonTapped = false})
+                        .presentationDetents([.fraction(0.4)])
+                }
+                
+                
+                
             }
             .navigationTitle("stats".localized())
+            .navigationBarItems(
+                trailing:
+                    Button {
+                        isAddDataButtonTapped.toggle()
+                    } label: {
+                        Text("Add Data")
+                    }
+            )
         }
         
     }
@@ -41,4 +88,11 @@ struct StatsView: View {
 
 #Preview {
     StatsView()
+}
+
+struct AddDataView: View {
+    var dismiss: () -> Void
+    var body: some View {
+        Text("Add Data button tapped")
+    }
 }
--- GrowWithMe/ViewModel/PhysicalDataViewModel.swift
+++ GrowWithMe/ViewModel/PhysicalDataViewModel.swift
@@ -14,8 +14,26 @@ class PhysicalDataViewModel: ObservableObject {
         PhysicalDataModel(weight: 4.6, length: 0.59, date: Date().addingTimeInterval(86400 * 10)),
         PhysicalDataModel(weight: 5, length: 0.62, date: Date().addingTimeInterval(86400 * 15)),
         PhysicalDataModel(weight: 5.5, length: 0.7, date: Date().addingTimeInterval(86400 * 20)),
-        PhysicalDataModel(weight: 6.2, length: 1.7, date: Date().addingTimeInterval(86400 * 25)),
+        PhysicalDataModel(weight: 6.2, length: 0.98, date: Date().addingTimeInterval(86400 * 25)),
+        PhysicalDataModel(weight: 7, length: 1.1, date: Date().addingTimeInterval(86400 * 30)),
+        PhysicalDataModel(weight: 8.2, length: 1.2, date: Date().addingTimeInterval(86400 * 35)),
+        PhysicalDataModel(weight: 10.6, length: 1.39, date: Date().addingTimeInterval(86400 * 36)),
+        PhysicalDataModel(weight: 12, length: 1.42, date: Date().addingTimeInterval(86400 * 40)),
+        PhysicalDataModel(weight: 12.5, length: 1.52, date: Date().addingTimeInterval(86400 * 45)),
+        PhysicalDataModel(weight: 13.2, length: 1.6, date: Date().addingTimeInterval(86400 * 55)),
+        PhysicalDataModel(weight: 15, length: 1.61, date: Date().addingTimeInterval(86400 * 56)),
+        PhysicalDataModel(weight: 17.2, length: 1.62, date: Date().addingTimeInterval(86400 * 60)),
+        PhysicalDataModel(weight: 19.6, length: 1.69, date: Date().addingTimeInterval(86400 * 65)),
+        PhysicalDataModel(weight: 20, length: 1.72, date: Date().addingTimeInterval(86400 * 70)),
+        PhysicalDataModel(weight: 21.5, length: 1.73, date: Date().addingTimeInterval(86400 * 75)),
+        PhysicalDataModel(weight: 23.2, length: 1.8, date: Date().addingTimeInterval(86400 * 80)),
         
     ]
+    
+    func calculateBMI(weight: Double, height: Double) -> Double {
+        guard height > 0 else { return 0 }
+        return weight / (height * height)
+    }
+    
 }
 
